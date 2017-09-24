ed428c0b4a60d018d2b04677d7476c2f6728a189
Syed Souban <syedsouban890@gmail.com>
2017-09-24 06:57:38 +0530

    Added profie ultimmate

--- app/src/main/java/sugoi/android/amazfun/ProfileActivity.java
+++ app/src/main/java/sugoi/android/amazfun/ProfileActivity.java
@@ -1,56 +1,61 @@
+
 package sugoi.android.amazfun;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.support.constraint.ConstraintLayout;
+        import android.support.constraint.ConstraintSet;
+        import android.support.design.widget.AppBarLayout;
+        import android.support.design.widget.CollapsingToolbarLayout;
+        import android.support.design.widget.TabLayout;
+        import android.support.v4.content.ContextCompat;
+        import android.support.v4.widget.NestedScrollView;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import org.w3c.dom.Text;
+        import static android.view.View.GONE;
+        import java.util.zip.Inflater;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.TabLayout;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.NestedScrollView;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+public class ProfileActivity extends Fragment {
 
-import org.w3c.dom.Text;
 
-import static android.view.View.GONE;
-
-public class ProfileActivity extends AppCompatActivity {
-
-    TextView sellerName;
-    TextView sellerBio;
     TextView sellerNamebp;
     TextView sellerBiobp;
     ImageView profilePicture;
-    ImageView profilePicturelp;
+
     TextView followersbp;
     TextView followingbp;
-    TextView followers;
-    TextView following;
-    ConstraintLayout constraintLayout;
+    public ProfileActivity() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return  inflater.inflate(R.layout.profile_page, container, false);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View v=getView();
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_page);
-        sellerName=(TextView) findViewById(R.id.sellername);
-        sellerBio=(TextView)findViewById(R.id.sellerbio);
+        sellerNamebp=(TextView)v. findViewById(R.id.sellerNamebp);
+        sellerBiobp=(TextView)v.findViewById(R.id.sellerbiobp);
+        profilePicture=(ImageView)v.findViewById(R.id.profile_image);
 
-        sellerNamebp=(TextView) findViewById(R.id.sellerNamebp);
-        sellerBiobp=(TextView)findViewById(R.id.sellerbiobp);
-        profilePicture=(ImageView)findViewById(R.id.profile_image);
-        profilePicturelp=(ImageView)findViewById(R.id.profile_imagelp);
-        followersbp=(TextView)findViewById(R.id.followersbp);
-        followingbp=(TextView)findViewById(R.id.followingbp);
-        followers=(TextView)findViewById(R.id.followers);
-        following=(TextView)findViewById(R.id.following);
-
-        constraintLayout=(ConstraintLayout)findViewById(R.id.cl);
-        CollapsingToolbarLayout cp=(CollapsingToolbarLayout)findViewById(R.id.collaptb);
+        followersbp=(TextView)v.findViewById(R.id.followersbp);
+        followingbp=(TextView)v.findViewById(R.id.followingbp);
+        //  constraintLayout=(ConstraintLayout)findViewById(R.id.cl);
+      /*  CollapsingToolbarLayout cp=(CollapsingToolbarLayout)findViewById(R.id.collaptb);
         cp.setContentScrimColor(ContextCompat.getColor(getApplicationContext(),R.color.black));
         NestedScrollView fsa=(NestedScrollView)findViewById(R.id.nstscmain);
         fsa.setFillViewport(true);
@@ -92,6 +97,7 @@ public class ProfileActivity extends AppCompatActivity {
                 }
             }
         });
-    }
+        */
 
+    }
 }
