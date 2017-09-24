71144f819fe8529f4762611e7015d09ab306179f
Shaik Adil <shaikadil86@gmail.com>
2017-09-24 07:06:30 +0530

    Added Support

--- app/src/main/java/sugoi/android/amazfun/ui/activities/SplashActivity.java
+++ app/src/main/java/sugoi/android/amazfun/ui/activities/SplashActivity.java
@@ -24,7 +24,6 @@ public class SplashActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_splash);
-
             mHandler = new Handler();
 
             mRunnable = new Runnable() {
